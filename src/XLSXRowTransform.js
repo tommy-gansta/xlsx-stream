e8906da6fa4a3fb4f30d7480e850f38b4df91695
Tommy <artem.anoshenko@gmail.com>
2020-09-23 21:21:17 +0000

    Update XLSXRowTransform.js
    avoid empty rows in table

--- src/XLSXRowTransform.js
+++ src/XLSXRowTransform.js
@@ -11,6 +11,8 @@ export default class XLSXRowTransform extends Transform {
      * Transform array to row string
      */
     _transform(row, encoding, callback) { // eslint-disable-line
+        if (!row || row.length == 0) callback();
+        
         const xlsxRow = Row(this.rowCount, row);
         this.rowCount++;
         callback(null, xlsxRow);
